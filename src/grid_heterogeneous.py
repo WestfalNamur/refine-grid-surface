7cbc313709b0ff19e9346dacf1ac58511de1a38f
stefan crummenerl <crummenerls@gmail.com>
2019-06-04 22:27:41 +0200

    fix 2d-heterogeneouse and add 3d-hetergeneouse uncluding jupyter plot

--- src/grid_heterogeneous.py
+++ src/grid_heterogeneous.py
@@ -3,9 +3,9 @@ import numpy as np
 
 def find_heterogeneous_cells(cell_coords, incr, condition):
 
-    if cell_coords.shape[0] == 2:
+    n_cells = cell_coords.shape[1]
 
-        n_cells = cell_coords.shape[1]
+    if cell_coords.shape[0] == 2:
 
         node_00 = np.array([
             cell_coords[0, :] - incr[0] / 2,
@@ -38,9 +38,83 @@ def find_heterogeneous_cells(cell_coords, incr, condition):
 
         boolean = scalar.astype(int) == condition
 
-        boolean_reshaped = np.reshape(boolean, (n_cells, 4))
+        return (
+            ~(np.all(boolean.reshape(4, n_cells) == True, axis=0)
+                | np.all(boolean.reshape(4, n_cells) == False, axis=0)),
+            np.all(boolean.reshape(4, n_cells) == True, axis=0),
+        )
 
-        return ~(
-            np.all(boolean_reshaped == True, axis=1)
-            | np.all(boolean_reshaped == False, axis=1)
+    if cell_coords.shape[0] == 3:
+
+        node_000 = np.array([
+            cell_coords[0, :] - incr[0] / 2,
+            cell_coords[1, :] - incr[1] / 2,
+            cell_coords[2, :] - incr[2] / 2,
+        ])
+
+        node_001 = np.array([
+            cell_coords[0, :] - incr[0] / 2,
+            cell_coords[1, :] - incr[1] / 2,
+            cell_coords[2, :] + incr[2] / 2,
+        ])
+
+        node_010 = np.array([
+            cell_coords[0, :] - incr[0] / 2,
+            cell_coords[1, :] + incr[1] / 2,
+            cell_coords[2, :] - incr[2] / 2,
+        ])
+
+        node_011 = np.array([
+            cell_coords[0, :] - incr[0] / 2,
+            cell_coords[1, :] + incr[1] / 2,
+            cell_coords[2, :] + incr[2] / 2,
+        ])
+
+        node_100 = np.array([
+            cell_coords[0, :] + incr[0] / 2,
+            cell_coords[1, :] - incr[1] / 2,
+            cell_coords[2, :] - incr[2] / 2,
+        ])
+
+        node_101 = np.array([
+            cell_coords[0, :] + incr[0] / 2,
+            cell_coords[1, :] - incr[1] / 2,
+            cell_coords[2, :] + incr[2] / 2,
+        ])
+
+        node_110 = np.array([
+            cell_coords[0, :] + incr[0] / 2,
+            cell_coords[1, :] + incr[1] / 2,
+            cell_coords[2, :] - incr[2] / 2,
+        ])
+
+        node_111 = np.array([
+            cell_coords[0, :] + incr[0] / 2,
+            cell_coords[1, :] + incr[1] / 2,
+            cell_coords[2, :] + incr[2] / 2,
+        ])
+
+        node_stack = np.hstack((
+            node_000,
+            node_001,
+            node_010,
+            node_011,
+            node_100,
+            node_101,
+            node_110,
+            node_111,
+        ))
+
+        scalar = np.sqrt((
+            node_stack[0, :] ** 2
+            + node_stack[1, :] ** 2
+            + node_stack[2, :] ** 2
+        ))
+
+        boolean = scalar.astype(int) == condition
+
+        return (
+            ~(np.all(boolean.reshape(8, n_cells) == True, axis=0)
+                | np.all(boolean.reshape(8, n_cells) == False, axis=0)),
+            np.all(boolean.reshape(8, n_cells) == True, axis=0),
         )
